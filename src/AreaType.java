69e0399b57ec9c86f0d04900b9afa0d5b27464a5
Oleksandr Shevchuk <Oleksandr Shevchuk>
2015-12-17 22:35:21 +0200

    Initial commit

---
+++ src/AreaType.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Oleksandr on 28.11.2015.
+ */
+public class AreaType {
+    private String name;
+    private float area;
+
+    public AreaType(String name, float area) {
+        this.name = name;
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+}
---
+++ src/Report.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Oleksandr on 28.11.2015.
+ */
+public class Report {
+    private int emergencyId;
+    private Date date;
+    private float radiation;
+    private String info;
+
+    public Report(int emergencyId, Date date, float radiation, String info) {
+        this.emergencyId = emergencyId;
+        this.date = date;
+        this.radiation = radiation;
+        this.info = info;
+    }
+
+    public int getEmergencyId() {
+        return emergencyId;
+    }
+
+    public void setEmergencyId(int emergencyId) {
+        this.emergencyId = emergencyId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public float getRadiation() {
+        return radiation;
+    }
+
+    public void setRadiation(float radiation) {
+        this.radiation = radiation;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
